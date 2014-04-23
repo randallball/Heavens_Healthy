c8051ae59d1e4c4b03710cdeff2c5a8802d89257
Randall Ball <16randall@gmail.com>
2014-04-23 16:15:20 -0600

    Initialize Heaven's Healthy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeavensHealthy::Application.config.secret_key_base = '648c9aa80218f83aff3287ef093ac3406d0aba552dd379eb55a4603f414f9d0f7c17af1e183014d237fde9f1a59b9546b58e90447201a0e97596ed41a3417282'
