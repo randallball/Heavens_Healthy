930814ef27ec4987e691d26001d0bbd856d1e639
Randall Ball <16randall@gmail.com>
2014-04-24 17:21:40 -0600

    Center nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Heaven's Healthy"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
